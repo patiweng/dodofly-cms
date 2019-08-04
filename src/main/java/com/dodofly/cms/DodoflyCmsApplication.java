7ff82732ce7dfa4ff596ee29652d5396644db16e
yang <1298605201@qq.com>
2019-08-04 21:33:08 +0800

    渡渡鸟飞翔cms后台管理系统

---
+++ src/main/java/com/dodofly/cms/DodoflyCmsApplication.java
@@ -0,0 +1,13 @@
+package com.dodofly.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DodoflyCmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DodoflyCmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dodofly/cms/DodoflyCmsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dodofly.cms;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DodoflyCmsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
